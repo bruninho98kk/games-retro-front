69e0584135c3b84d245de9114fe136950044ddaf
Bruno Quintino <brunoquintino.tds2.senai@gmail.com>
2025-05-15 09:26:01 -0300

    feat: create search inputs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.games}>
       <h1>Games Retrô</h1>
       <p>Recordações de jogos retrô</p>
     </main>
